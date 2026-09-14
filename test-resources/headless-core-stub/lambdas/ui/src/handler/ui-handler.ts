e2add434b5b6835c52948d10c1ff1166c25ed348
Nathaniel Steers <nathaniel.steers@digital.cabinet-office.gov.uk>
2026-09-14 16:44:30 +0100

    [squash] force serverlessExpress to treat everything as text

--- test-resources/headless-core-stub/lambdas/ui/src/handler/ui-handler.ts
+++ test-resources/headless-core-stub/lambdas/ui/src/handler/ui-handler.ts
@@ -1,4 +1,4 @@
 import serverlessExpress from "@codegenie/serverless-express";
 import { createApp } from "../app";
 
-export const lambdaHandler = serverlessExpress({ app: createApp() });
+export const lambdaHandler = serverlessExpress({ app: createApp(), binarySettings: { isBinary: false } });
